f9f3d6aea2e33d11fb5efa144846f9b53184c834
a19phith <philip2leo@gmail.com>
2020-04-08 09:26:44 +0200

    Updated buttons, created a internal webpage.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -17,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
 
     public void showExternalWebPage(){
-        // TODO: Add your code for showing external web page here
+        myWebView.loadUrl("https://wwwlab.iit.his.se/a19phith/mobilappdesign/prototyp/a19phith.app.html.html");
     }
 
     public void showInternalWebPage(){
-        // TODO: Add your code for showing internal web page here
+        myWebView.loadUrl("file:///android_asset/index.html");
     }
 
     @Override
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         myWebView.getSettings().setJavaScriptEnabled(true);
 
+        //showExternalWebPage();
+        showInternalWebPage();
 
         /*
         * Rename your App. Tip: Values->Strings
@@ -87,11 +89,13 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
             Log.d("==>","Will display external web page");
+            showExternalWebPage();
             return true;
         }
 
         if (id == R.id.action_internal_web) {
             Log.d("==>","Will display internal web page");
+            showInternalWebPage();
             return true;
         }
 
